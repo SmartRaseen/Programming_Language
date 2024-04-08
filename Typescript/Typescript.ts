ee11a40293e5703b788f9338b94068dcf3c48d67
Hameed Sulthan <55388326+SmartRaseen@users.noreply.github.com>
2024-04-08 12:17:09 +0530

    Initial commit

---
+++ Typescript/Typescript.ts
@@ -0,0 +1,13 @@
+Why TypeScript?
+JavaScript is a dynamic programming language with no type system. 
+JavaScript provides primitive types like string, number, object, etc., but it doesn't check assigned values. 
+JavaScript variables are declared using the var keyword, and it can point to any value. 
+JavaScript doesn't support classes and other object-oriented features (ECMA2015 supports it). 
+So, without the type system, it is not easy to use JavaScript to build complex applications with large teams working on the same code.
+
+
+How to use TypeScript?
+TypeScript code is written in a file with .ts extension and then compiled into JavaScript using the TypeScript compiler. 
+A TypeScript file can be written in any code editor. A TypeScript compiler needs to be installed on your platform. 
+Once installed, the command tsc <filename>.ts compiles the TypeScript code into a plain JavaScript file. 
+JavaScript files can then be included in the HTML and run on any browser.
